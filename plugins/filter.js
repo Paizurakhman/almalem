4f257be45a0213bffb77d79dff41840adef1f5d6
Paizurakhman <170107091@stu.sdu.edu.kz>
2021-05-25 12:51:25 +0600

    filter

--- plugins/filter.js
+++ plugins/filter.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 
-Vue.filter('truncate', val => {
-  val = val.toString()
-  if (val.length >= 48) {
-    return val.slice(0, 48) + "..."
+Vue.filter('truncate', value => {
+  value = value.toString()
+  if (value.length >= 48) {
+    return value.slice(0, 48) + "..."
   }
-  return val
+  return value
 })
