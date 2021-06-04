422922c7762254c9c78b576ea3e79e19294d182e
Paizurakhman <170107091@stu.sdu.edu.kz>
2021-06-04 16:22:00 +0600

    responsive

--- pages/cart/checkout.vue
+++ pages/cart/checkout.vue
@@ -50,50 +50,23 @@
                       </div>
                     </div>
                       <div class="home_address">
-                        <input
-                          type="text"
-                          placeholder="Дом"
-                          v-model="address.house"
-                        />
-                        <input
-                          type="text"
-                          placeholder="Корпус"
-                          v-model="address.building"
-                        />
-                        <input
-                          type="text"
-                          placeholder="Подъезд"
-                          v-model="address.entrance"
-                        />
-                        <input
-                          type="text"
-                          placeholder="Этаж"
-                          v-model="address.floor"
-                        />
-                        <input
-                          type="text"
-                          placeholder="Квартира"
-                          v-model="address.apartment"
-                        />
+                        <input class="custom_input" type="text" placeholder="Дом" v-model="address.house">
+                        <input class="custom_input" type="text" placeholder="Корпус" v-model="address.building">
+                        <input class="custom_input" type="text" placeholder="Подъезд" v-model="address.entrance">
+                        <input class="custom_input" type="text" placeholder="Этаж" v-model="address.floor">
+                        <input class="custom_input" type="text" placeholder="Квартира" v-model="address.apartment">
                       </div>
                     <div class="i_title">
                       <p>Комментарии к заказу</p>
                     </div>
-                    <textarea
-                      placeholder="Комментарии"
-                      v-model="main_info.comment"
-                    ></textarea>
+                    <textarea placeholder="Комментарии" v-model="main_info.comment"></textarea>
                   </div>
                   <div class="col-xl-5">
                     <div class="input_title">
                       <p>Ваш заказ</p>
                     </div>
                     <div class="address_card" v-if="cartData">
-                      <div
-                        class="ordering_card"
-                        v-for="product in cartData.products"
-                        :key="product.id"
-                      >
+                      <div class="ordering_card" v-for="product in cartData.products" :key="product.id">
                         <div class="row">
                           <div class="col-xl-3 col-3">
                             <img class="order" :src="'http://cdn.astudiodigital.ru/' + product.images.image" alt="">
@@ -103,21 +76,14 @@
                           </div>
                           <div class="col-xl-2 col-3">
                             <div class="item_amount">
-                              <p class="count">{{ product.count }} шт</p>
-                              <p class="price">
-                                {{ product.count * product.price }}₸
-                              </p>
+                              <p class="count">1 шт</p>
+                              <p class="price">{{product.price}}₸</p>
                             </div>
                           </div>
                           <div class="col-xl-2 col-2">
                             <div class="table_wrapper">
                               <div class="table_action">
-                                <img
-                                  @click="deleteCartProduct(product.id)"
-                                  class="delete"
-                                  src="~/assets/icon/clear.svg"
-                                  alt=""
-                                />
+                                <img @click="deleteCartProduct(product.id)" class="delete" src="~/assets/icon/clear.svg" alt="">
                               </div>
                             </div>
                           </div>
@@ -126,9 +92,7 @@
                     </div>
                     <div class="card_footer">
                       <div class="sum">
-                        <p>
-                          Итого: <span class="b_text">{{ totalPrice }} ₸</span>
-                        </p>
+                        <p>Итого: <span class="b_text">1400 ₸</span></p>
                       </div>
                       <div class="action">
                         <button class="btn btn_main">Перейти к оплате</button>
@@ -146,137 +110,104 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions } from 'vuex'
 export default {
-  name: "checkout",
+  name: 'checkout',
   data() {
     return {
       cartData: null,
       cart: null,
       regions: null,
       cities: null,
-      count: 1,
       main_info: {
-        name: "",
-        phone: "",
-        email: "",
-        comment: "",
-        token: null,
+        name: '',
+        phone: '',
+        email: '',
+        comment: '',
+        token: null
       },
       address: {
-        region: "",
-        city: "",
-        street: "",
-        house: "",
-        building: "",
-        entrance: "",
-        floor: "",
-        apartment: "",
-      },
-    };
+        region: '',
+        city: '',
+        street: '',
+        house: '',
+        building: '',
+        entrance: '',
+        floor: '',
+        apartment: ''
+      }
+    }
   },
   computed: {
-    get_cart() {
+    get_cart () {
       if (this.cart.length) {
-        this.cart = JSON.parse(localStorage.getItem("cart"));
-        this.$axios
-          .$get("card-product", {
-            params: {
-              lang: this.$store.state.lang,
-              product_id: this.cart.map((item) => {
-                return item.id;
-              }),
-            },
-          })
-          .then((res) => {
-            this.cartData = res;
-            this.cart.filter((item, index) => {
-              this.cartData.products.forEach((element, index) => {
-                if (item.id === element.id) {
-                  this.$set(
-                    this.cartData.products[index],
-                    "count",
-                    Number(item.count)
-                  );
-                }
-              });
-            });
-          });
-      }
-    },
-
-    totalPrice() {
-      let result = [];
-      if (this.cartData) {
-        this.cartData.products.filter((item, i) => {
-          for (let i of JSON.parse(localStorage.getItem("cart"))) {
-            if (item.id === i.id) {
-              result.push(item.price * item.count);
-            }
-          }
-        });
-      }
-
-      if (result.length) {
-        result = result.reduce(function (sum, el) {
-          return sum + el;
-        });
-      }
-
-      return result;
-    },
-  },
-  async mounted() {
-    this.cart = JSON.parse(localStorage.getItem("cart"));
-    if (this.cart) {
-      this.get_cart;
-    }
-    await this.$axios
-      .$get("get-region?lang=" + this.$store.state.lang)
-      .then((res) => {
-        this.regions = res.regions;
-      });
-  },
-  methods: {
-    ...mapActions(["CART_ACTION"]),
-    async getCity() {
-      this.address.city = "";
-      await this.$axios
-        .$get("get-city", {
+        this.$axios.$get('card-product', {
           params: {
             lang: this.$store.state.lang,
-            region_id: this.address.region,
-          },
+            product_id: this.cart.map(item => {
+              return item.id
+            })
+          }
         })
-        .then((res) => {
-          this.cities = res.cities;
-        });
+          .then(res => {
+            this.cartData = res
+          })
+      }else {
+        this.cartData = null
+      }
+    }
+  },
+  async mounted() {
+    this.cart = JSON.parse(localStorage.getItem('cart'))
+    if (this.cart) {
+      this.get_cart
+    }
+    await this.$axios.$get('get-region?lang=' + this.$store.state.lang)
+    .then(res => {
+      this.regions = res.regions
+    })
+  },
+  methods: {
+    ...mapActions([
+      'CART_ACTION'
+    ]),
+    async getCity() {
+      this.address.city = ''
+      await this.$axios.$get('get-city', {
+        params: {
+          lang: this.$store.state.lang,
+          region_id: this.address.region
+        }
+      })
+      .then(res => {
+        this.cities = res.cities
+      })
     },
-    deleteCartProduct(id) {
-      this.cart = this.cart.filter((rm) => {
-        return rm.id !== id;
-      });
-      localStorage.setItem("cart", JSON.stringify(this.cart));
+    deleteCartProduct (id) {
+      this.cart = this.cart.filter(rm => {
+        return rm.id !== id
+      })
+      localStorage.setItem('cart', JSON.stringify(this.cart))
 
-      this.get_cart;
-      this.CART_ACTION();
+      this.get_cart
+      this.CART_ACTION()
     },
     async handleBuy() {
-      this.main_info.token = localStorage.getItem("token");
-      await this.$axios
-        .$post("get-order", {
-          main_info: this.main_info,
-          products: this.cart,
-          address: this.address,
-        })
-        .then((res) => {
-          // window.location.href = res.url
-          window.open(res.url, "_self");
-        });
+      this.main_info.token = localStorage.getItem('token')
+      await this.$axios.$post('get-order', {
+        main_info: this.main_info,
+        products: this.cart,
+        address: this.address
+      })
+      .then(res => {
+        // window.location.href = res.url
+        window.open(res.url, '_self')
+      })
     },
-  },
-};
+  }
+}
 </script>
 
 <style lang="scss">
+
 </style>
